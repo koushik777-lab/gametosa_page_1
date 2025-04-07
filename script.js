dd9eda83a07ff4eba53562a845cc2589880efc4d
koushik sarkar <koushiksarkar741777@gmail.com>
2025-04-07 22:33:46 +0530

    Merge remote-tracking branch 'origin/main' into local

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let tl = gsap.timeline()
 
-tl.from(["#logo_container img", ".line2", ".line3"], {
+tl.from(["#logo_container img",".line1", ".line2", ".line3"], {
     y: -30,
     x: -20,
     scale: 0.2,
     opacity: 0,
     stagger: 0.2,
   });
+
+
   
 
